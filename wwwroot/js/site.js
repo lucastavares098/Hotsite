ab89b4e42a23eeb3ae2b36b1d1b06dc04460ea6b
lucastavares098 <145068285+lucastavares098@users.noreply.github.com>
2023-11-23 19:33:50 -0300

    Update script

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,24 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$(document).ready(function () {
+
+// Tempo do alerta
+    setTimeout(function () {
+        $('#tempMessage').fadeOut('fast');
+    }, 3000); // 3 segundos
+
+    // Abrir modal de eventos
+    $("#eventos").click(function () {
+        $("#eventosModal").modal('show');
+    });
+// Abrir modal de apoiadores
+    $("#apoiadores").click(function () {
+        $("#apoiadoresModal").modal('show');
+    });
+// Abrir modal de vida saudavel
+    $("#saudavel").click(function () {
+        $("#saudavelModal").modal('show');
+    });
+});
